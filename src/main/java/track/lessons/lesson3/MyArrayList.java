2dc8bcb10cb82eb55c18a1593f321a373cf169a0
Busovsky Oleg <buslovsky@okbsapr.ru>
2017-03-16 03:31:06 +0300

    class MyArrayList was realesed

--- src/main/java/track/lessons/lesson3/MyArrayList.java
+++ src/main/java/track/lessons/lesson3/MyArrayList.java
@@ -9,33 +9,68 @@ import java.util.NoSuchElementException;
  * - без аргументов - создает внутренний массив дефолтного размера на ваш выбор
  * - с аргументом - начальный размер массива
  */
-public class MyArrayList extends List {
+class MyArrayList extends List {
 
-    public MyArrayList() {
 
+    private int realSize = 10;
+    private int [] array;
+
+    MyArrayList() {
+        array = new int[realSize];
     }
 
-    public MyArrayList(int capacity) {
-
+    MyArrayList(int capacity) {
+        size = capacity;
+        if (realSize < capacity) {
+            realSize = capacity;
+        }
+        array = new int [realSize];
     }
 
     @Override
     void add(int item) {
-
+        if (realSize == size) {
+            float increase = (float) 1.5;
+            int [] tmp = new int[size];
+            System.arraycopy(array, 0, tmp, 0, size);
+            realSize = (int)(size * increase);
+            array = new int[realSize];
+            System.arraycopy(tmp, 0, array, 0, size);
+        }
+        array[size] = item;
+        size++;
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx < 0 || idx >= size) {
+            throw new NoSuchElementException();
+        }
+        if (size == 1) {
+            size--;
+            return array[size];
+        }
+        int [] tmp = new int [size - 1];
+        int tmpIter = 0;
+        for (int i = 0 ; i < size; i++) {
+            if (i == idx) {
+                continue;
+            }
+            tmp[tmpIter] = array[i];
+            tmpIter++;
+        }
+        int num = array[idx];
+        size--;
+        System.arraycopy(tmp, 0, array, 0, size);
+        return num;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx < 0 || idx >= size) {
+            throw new NoSuchElementException();
+        }
+        return array[idx];
     }
 
-    @Override
-    int size() {
-        return 0;
-    }
 }
